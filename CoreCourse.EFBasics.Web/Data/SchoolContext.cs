1075138aa3bbbb2d21c09d2f631d5fb24386bede
sigged <sderdeyn@gmail.com>
2018-01-31 01:58:21 +0100

    Added entities, context and connectionstring

---
+++ CoreCourse.EFBasics.Web/Data/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using CoreCourse.EFBasics.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCourse.EFBasics.Web.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+
+}
---
+++ CoreCourse.EFBasics.Web/Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreCourse.EFBasics.Web.Entities
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Teacher Lecturer { get; set; }
+    }
+}
