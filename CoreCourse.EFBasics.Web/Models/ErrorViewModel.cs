c173d6a90f378271638da0825a6e3ce123f4461f
sigged <sderdeyn@gmail.com>
2018-01-31 01:34:42 +0100

    Initial project creation

---
+++ CoreCourse.EFBasics.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreCourse.EFBasics.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
