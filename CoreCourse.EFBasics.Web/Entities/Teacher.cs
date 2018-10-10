ccd4ee2c17dbcdcdc4e43451aae8b401d6b1c6ab
sigged <sderdeyn@gmail.com>
2018-10-11 00:19:32 +0200

    Added entities, context and connectionstring

---
+++ CoreCourse.EFBasics.Web/Entities/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CoreCourse.EFBasics.Web.Entities
+{
+    public class Teacher
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal YearlyWage { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
