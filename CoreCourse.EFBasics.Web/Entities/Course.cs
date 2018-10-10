9ef33f1c9cdaf50c93db39d60e04038ddba55a4b
sigged <sderdeyn@gmail.com>
2018-10-11 01:13:58 +0200

    Added data annotations using attributes

--- CoreCourse.EFBasics.Web/Entities/Course.cs
+++ CoreCourse.EFBasics.Web/Entities/Course.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoreCourse.EFBasics.Web.Entities
 {
     public class Course
     {
+        [Key]
         public Guid Id { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [Required]
         public Teacher Lecturer { get; set; }
+
     }
 }
